8c8406df097316193071c862e3daca3c2100db45
Shatrudhan Kumar Sharma <shatrudhansharma325@gmail.com>
2025-08-28 13:50:31 +0530

    Day-1 Learning Dart

---
+++ 01.BasicDart/variable_DataType.dart
@@ -0,0 +1,18 @@
+
+
+void variable() {
+  int a = 10;
+  int b = 20;
+  int c = a+b;
+  String name = "akash";
+  print(c);
+  print(name);
+  
+}
+void main(){
+  int a = 10;
+  String name = "Ram";
+  print(a);
+  print(name);
+  variable();
+}
