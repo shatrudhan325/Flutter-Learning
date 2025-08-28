9da57fd8588968beadce3d3a4075be6febedb2c0
Shatrudhan Kumar Sharma <shatrudhansharma325@gmail.com>
2025-08-28 16:54:26 +0530

    Day - 1 Learning Dart

---
+++ 01.BasicDart/if_else.dart
@@ -0,0 +1,36 @@
+void main(){
+  var i = 5;
+  if (i < 6) {
+    print("condition is work properly");
+  }else{
+    print("condition is not working");
+  }
+  int day = 6;
+
+  switch (day) {
+    case 1:
+      print("Monday");
+      break;
+    case 2:
+      print("Tuesday");
+      break;
+    case 3:
+      print("Wednesday");
+      break;
+    case 4:
+      print("Thursday");
+      break;
+    case 5:
+      print("Friday");
+      break;
+    case 6:
+      print("Saturday");
+      break;
+    case 7:
+      print("Sunday");
+      break;
+    default:
+      print("Invalid Day");
+  }
+}
+
--- 01.BasicDart/loop.dart
+++ 01.BasicDart/loop.dart
@@ -2,4 +2,14 @@ void main(){
   for (var i = 0; i < 5; i++) {
     print("ha ji mai loop hu.");
   }
+  int i = 1;
+  int sum = 0;
+
+  while (i <= 5) {
+    sum += i;
+    i++;
+  }
+
+  print("Sum of all five member is = $sum");
 }
+
