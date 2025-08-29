70445fb5b2d9914763d78e134d7a4cd4410160de
Shatrudhan Kumar Sharma <shatrudhansharma325@gmail.com>
2025-08-29 12:04:26 +0530

    Day - 2 Learning Dart

---
+++ 02_Function_and_Advanced/function.dart
@@ -0,0 +1,14 @@
+void main(){
+  akash Akash = new akash();
+  int result = Akash.multipal(2);
+  print(result);
+  //Example of class
+  
+
+}
+class akash {
+  int multipal(int n){
+  return n*10;
+}
+}
+//return type function/user define function.
